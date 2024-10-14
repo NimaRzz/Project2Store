e5e6492e8a306c4a265f0057cbab560269e5d4b9
Nima Rezaee <nyma0458@gmail.com>
2024-10-14 20:01:35 +0330

    Add Controller And Views And Change Kestrel Port
    Add Controller
    -HomeController
    
     And Views
    -Home Index
    -ViewImports
    -ViewStart
    
     And Change Kestrel Port
    -Changed To 5000

--- Project2Store/Project2Store.ShopUI/obj/Debug/net6.0/Project2Store.ShopUI.AssemblyInfo.cs
+++ Project2Store/Project2Store.ShopUI/obj/Debug/net6.0/Project2Store.ShopUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project2Store.ShopUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8594dd77008b550eaa304d6756bbd2f95856d99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53cf25d04c1c01002bdb2f461ad476a9d4c95505")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project2Store.ShopUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project2Store.ShopUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
