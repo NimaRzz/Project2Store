4b916def2cad65e74d336a3c2ce30d225b4764a6
Nima Rezaee <nyma0458@gmail.com>
2024-10-15 14:35:21 +0330

    Add Basket
    Add
    -Basket

---
+++ Project2Store/Project2Store.ShopUI/Models/Basket.cs
@@ -0,0 +1,42 @@
+﻿namespace Project2Store.ShopUI.Models;
+
+    public class Basket
+    {
+        public List<BasketItem> _Items = new();
+
+        public void Add(Product product, int quantity)
+        {
+            var basketItem = _Items.Where(p => p.Product.Id == product.Id).FirstOrDefault();
+            if (basketItem != null)
+            {
+                basketItem.Quantity += quantity;
+            }
+            else
+            {
+                _Items.Add(new BasketItem()
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+        }
+
+        public void Remove(Product product) =>
+            _Items.RemoveAll(p => p.Product.Id == product.Id);
+
+        public int TotalPrice() =>
+            _Items.Sum(p => p.Product.Price * p.Quantity);
+
+        public void Clear() => _Items.Clear();
+
+        public IEnumerable<BasketItem> Items => Items;
+    }
+
+    public class BasketItem
+    {
+        public long Id { get; set; }
+
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
